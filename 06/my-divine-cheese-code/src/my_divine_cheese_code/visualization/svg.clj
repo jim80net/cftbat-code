2928059f7e5e9cd7a7f6f84912dca83bbbec1c27
Jim Park <jimp@scribd.com>
2019-10-25 22:15:11 -0700

    my_divine_cheese_code

---
+++ 06/my-divine-cheese-code/src/my_divine_cheese_code/visualization/svg.clj
@@ -0,0 +1,24 @@
+(ns my-divine-cheese-code.visualization.svg
+  (:require [clojure.string :as s])
+  (:refer-clojure :exclude [min max]))
+
+(defn latlng->point
+  "Convert lat/lng to comma-separated string"  
+  [latlng]
+  (str (:lat latlng) "," (:lng latlng)))
+
+(defn points
+  [locations]
+  (clojure.string/join " " (map latlng->point locations)))
+
+(defn comparator-over-maps
+  [comparison-fn ks]
+  (fn [maps]
+    (zipmap ks 
+            (map (fn [k]
+                   (apply comparison-fn (map k maps)))
+                 ks))))
+
+(def min (comparator-over-maps clojure.core/min [:lat :lng]))
+(def max (comparator-over-maps clojure.core/max [:lat :lng]))
+
